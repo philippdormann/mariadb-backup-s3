6b5c4bdf371f3dc59ba011f939f78d4948254b73
Philipp Dormann <philipp@philippdormann.de>
2023-07-21 14:56:08 +0200

    feat!: move filename to unix timestamp

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ get_date () {
 : ${DB_PORT:='3306'}
 : ${DB_USER:?'Specify a DB_USER variable'}
 : ${DB_PASSWORD:?'You must specify the DB_PASSWORD variable'}
-: ${FILE_PREFIX:='backup-'}
+: ${FILE_PREFIX:=''}
 : ${S3_BUCKET:?'You must specify the S3_BUCKET variable'}
 : ${S3_SERVER:?'You must specify the S3_SERVER variable'}
 : ${S3_ACCESS:?'You must specify the S3_ACCESS variable'}
@@ -22,9 +22,9 @@ mc ilm rule add --expire-days ${KEEP_DAYS}  myminio/${S3_BUCKET}
 # mc retention set --default GOVERNANCE 7  myminio/${S3_BUCKET}
 
 dobackup() {
-    FILENAME=`date +%Y-%m-%d_%H-%M-%S`.sql.gz
+    FILENAME=`date +%s`.sql.gz
     echo "$(get_date) MariaDB backup $FILENAME started"
-    mariadb-dump --host=${DB_HOST} --port=${DB_PORT} --user=${DB_USER} --password=${DB_PASSWORD} --all-databases | pigz -9 | mc pipe --tags "type=backup" myminio/${S3_BUCKET}/${FILE_PREFIX}-${FILENAME}
+    mariadb-dump --host=${DB_HOST} --port=${DB_PORT} --user=${DB_USER} --password=${DB_PASSWORD} --all-databases | pigz -9 | mc pipe --tags "type=backup" myminio/${S3_BUCKET}/${FILE_PREFIX}${FILENAME}
     echo "$(get_date) MariaDB backup $FILENAME completed"
 }
 
